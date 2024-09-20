5c8cb0d176b447b0e7da7d1da28807c6543f9c3b
Pedro Henrique Marques <fmhenriquepedro1998@gmail.com>
2024-09-20 00:05:47 -0300

    20/09
    Lifes System Completed

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var ui: CanvasLayer = $UI
-@onready var respawn_point: Marker2D = $RespawnPoint
+
 
 
 
 
 func _ready() -> void:
+	ui.loadUi()
+	print("Dados Carregados por game _ready. Vidas:" + str(ui.ui_lifes))
 	ui.show_message("A sua Jornada se inicia!")
